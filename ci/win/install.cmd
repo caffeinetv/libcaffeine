42c42b0cf80212b2f8e8d5537fda4dcba5b5861d
Michael Fabian 'Xaymar' Dirks <info@xaymar.com>
2019-04-22 22:07:03 +0200

    LIB-48: Support for AppVeyor Continuous Integration
    Adds AppVeyor to our support continuous integration services, which will help us determine bad, or outright broken, commits and PRs. The integration relies on a few pre-built binaries that we are using for quicker build times.
    
    Currently only does building, format checking, testing and more will come at a later date.

---
+++ ci/win/install.cmd
@@ -0,0 +1,29 @@
+@ECHO OFF
+CALL "%~dp0\bootstrap"
+
+REM Update Submodules (if there are any)...
+ECHO Updating submodules...
+CALL git submodule update --init --force --recursive 2>NUL 1>NUL
+
+REM Download dependencies
+IF NOT EXIST "%ROOT%\.cache" (
+	MKDIR "%ROOT%\.cache"
+)
+
+ECHO Updating libcurl...
+IF EXIST %ROOT%\.cache\obs_deps.zip (
+	curl -kL "%OBS_DEPENDENCIES_URL%" -f --retry 5 -o "%ROOT%\.cache\obs_deps.zip" -z "%ROOT%\.cache\obs_deps.zip"
+) ELSE (
+	curl -kL "%OBS_DEPENDENCIES_URL%" -f --retry 5 -o "%ROOT%\.cache\obs_deps.zip"
+)
+7z x -y -o"%ROOT%\.cache\obs_deps" "%ROOT%\.cache\obs_deps.zip"
+
+ECHO Updating WebRTC...
+IF EXIST "%ROOT%\.cache\webrtc.7z" (
+	curl -kL "%WEBRTC_URL%" -f --retry 5 -o "%ROOT%\.cache\webrtc.7z" -z "%ROOT%\.cache\webrtc.7z"
+) ELSE (
+	curl -kL "%WEBRTC_URL%" -f --retry 5 -o "%ROOT%\.cache\webrtc.7z"
+)
+7z x -y -o"%ROOT%\.cache\webrtc" "%ROOT%\.cache\webrtc.7z"
+
+ECHO Done.
