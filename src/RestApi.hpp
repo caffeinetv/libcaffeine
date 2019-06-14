d217781afc8f36abdeaf3f09dcc459079ded0c1f
Daniel Resnick <danielzresnick@gmail.com>
2019-06-14 10:14:57 -0700

    Remove reyes-v4 types and request functions

--- src/RestApi.hpp
+++ src/RestApi.hpp
@@ -88,85 +88,6 @@ namespace caff {
         int sdpMLineIndex;
     };
 
-    struct FeedStream {
-        optional<std::string> id;
-        optional<std::string> sourceId;
-        optional<std::string> url;
-        optional<std::string> sdpOffer;
-        optional<std::string> sdpAnswer;
-    };
-
-    struct FeedCapabilities {
-        bool video;
-        bool audio;
-    };
-
-    enum class ContentType { Game, User };
-
-    struct FeedContent {
-        std::string id;
-        ContentType type;
-    };
-
-    struct Feed {
-        std::string id;
-        optional<std::string> clientId;
-        optional<std::string> role;
-        optional<std::string> description;
-        optional<caff_ConnectionQuality> sourceConnectionQuality;
-        optional<double> volume;
-        optional<FeedCapabilities> capabilities;
-        optional<FeedContent> content;
-        optional<FeedStream> stream;
-    };
-
-    struct Stage {
-        optional<std::string> id;
-        optional<std::string> username;
-        optional<std::string> title;
-        optional<std::string> broadcastId;
-        optional<bool> upsertBroadcast;
-        optional<bool> live;
-        optional<std::map<std::string, Feed>> feeds;
-    };
-
-    struct Client {
-        std::string id;
-        bool headless = true;
-        bool constrainedBaseline = false;
-    };
-
-    struct StageRequest {
-        Client client;
-        optional<std::string> cursor;
-        optional<Stage> stage;
-
-        StageRequest(std::string username, std::string clientId) {
-            client.id = std::move(clientId);
-            stage = Stage{};
-            stage->username = std::move(username);
-        }
-    };
-
-    struct StageResponse {
-        std::string cursor;
-        std::chrono::milliseconds retryIn;
-        optional<Stage> stage;
-    };
-
-    struct DisplayMessage {
-        std::string title;
-        optional<std::string> body;
-    };
-
-    struct FailureResponse {
-        std::string type;
-        optional<std::string> reason;
-        optional<DisplayMessage> displayMessage;
-    };
-
-    using StageResponseResult = variant<StageResponse, FailureResponse>;
-
     using ScreenshotData = std::vector<uint8_t>;
 
     struct HeartbeatResponse {
@@ -192,12 +113,6 @@ namespace caff {
             std::vector<caff::IceInfo> const & candidates,
             std::string const & streamUrl,
             SharedCredentials & credentials);
-
-    bool requestStageUpdate(
-            StageRequest & request,
-            SharedCredentials & creds,
-            std::chrono::milliseconds * retryIn,
-            bool * isOutOfCapacity);
 
     void sendWebrtcStats(SharedCredentials & creds, Json const & report);
 
