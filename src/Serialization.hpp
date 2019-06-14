ad8d284037bb0e058987b2e2b98c3b963c1fc0e5
Daniel Resnick <danielzresnick@gmail.com>
2019-06-14 10:14:57 -0700

    Remove reyes-v4 types and request functions

--- src/Serialization.hpp
+++ src/Serialization.hpp
@@ -48,8 +48,6 @@ namespace caff {
         }
     }
 
-    NLOHMANN_JSON_SERIALIZE_ENUM(ContentType, { { ContentType::Game, "game" }, { ContentType::User, "user" } })
-
     void from_json(Json const & json, Credentials & credentials);
 
     void from_json(Json const & json, UserInfo & userInfo);
@@ -58,32 +56,7 @@ namespace caff {
 
     void to_json(Json & json, IceInfo const & iceInfo);
 
-    void to_json(Json & json, Client const & client);
-
-    void to_json(Json & json, FeedCapabilities const & capabilities);
-    void from_json(Json const & json, FeedCapabilities & capabilities);
-
-    void to_json(Json & json, FeedContent const & content);
-    void from_json(Json const & json, FeedContent & content);
-
-    void to_json(Json & json, FeedStream const & stream);
-    void from_json(Json const & json, FeedStream & stream);
-
-    void to_json(Json & json, Feed const & feed);
-    void from_json(Json const & json, Feed & feed);
-
-    void to_json(Json & json, Stage const & stage);
-    void from_json(Json const & json, Stage & stage);
-
-    void to_json(Json & json, StageRequest const & request);
-
-    void from_json(Json const & json, StageResponse & response);
-
     void from_json(Json const & json, HeartbeatResponse & response);
-
-    void from_json(Json const & json, DisplayMessage & message);
-
-    void from_json(Json const & json, FailureResponse & response);
 
     // The WebRTC types fail nlohmann's "compatibility" checks when using the to_json overload, either as a free
     // function or as an adl_serializer specialization
