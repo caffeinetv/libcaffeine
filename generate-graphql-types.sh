3676615ac879e06375a9cf9d868c12d082c96d72
Daniel Resnick <danielzresnick@gmail.com>
2019-06-18 11:26:12 -0700

    Update graphql type generation script to allow custom domains. It uses production by default.

--- generate-graphql-types.sh
+++ generate-graphql-types.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 # Introspects and generates client code for the realtime graphql endpoint.
 # Assumes caffql, curl, and jq are in your path.
+# Set LIBCAFFEINE_DOMAIN to use a environment other than production.
 
 set -e
 
-CREDENTIAL=$(curl "https://api.staging.caffeine.tv/v1/credentials/anonymous" | jq -j .credential)
+LIBCAFFEINE_DOMAIN="${LIBCAFFEINE_DOMAIN:-caffeine.tv/}"
+
+echo $LIBCAFFEINE_DOMAIN
+
+CREDENTIAL=$(curl "https://api.${LIBCAFFEINE_DOMAIN}v1/credentials/anonymous" | jq -j .credential)
 
 curl \
     -X POST \
@@ -12,7 +17,7 @@ curl \
     -H "X-Credential: ${CREDENTIAL}" \
     --data '{"query": "query IntrospectionQuery { __schema { queryType { name } mutationType { name } subscriptionType { name } types { ...FullType } directives { name description locations args { ...InputValue } } } } fragment FullType on __Type { kind name description fields(includeDeprecated: true) { name description args { ...InputValue } type { ...TypeRef } isDeprecated deprecationReason } inputFields { ...InputValue } interfaces { ...TypeRef } enumValues(includeDeprecated: true) { name description isDeprecated deprecationReason } possibleTypes { ...TypeRef } } fragment InputValue on __InputValue { name description type { ...TypeRef } defaultValue } fragment TypeRef on __Type { kind name ofType { kind name ofType { kind name ofType { kind name ofType { kind name ofType { kind name ofType { kind name ofType { kind name } } } } } } } }"}' \
     -o schema.json \
-    https://realtime.staging.caffeine.tv/public/graphql/query
+    https://realtime.${LIBCAFFEINE_DOMAIN}public/graphql/query
 
 set +e
 
