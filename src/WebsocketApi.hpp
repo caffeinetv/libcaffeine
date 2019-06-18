3a759d2c215bee7499f018a87d1e2ea15e8fdbae
Daniel Resnick <danielzresnick@gmail.com>
2019-06-18 15:11:06 -0700

    Graphql subscription updates.
    Convert graphqlSubscription from function to class that manages an underlying connection.
    The subscription class will automatically refresh credentials if needed.
    Retry subscription failures with backoff.

--- src/WebsocketApi.hpp
+++ src/WebsocketApi.hpp
@@ -39,61 +39,124 @@ namespace caff {
         std::thread clientThread;
     };
 
-    template <typename OperationField, typename... Args>
-    optional<WebsocketClient::Connection> graphqlSubscription(
-            WebsocketClient & client,
-            std::string const & label,
-            std::function<void(caffql::GraphqlResponse<typename OperationField::ResponseData>)> messageHandler,
-            std::function<void(WebsocketClient::ConnectionEndType)> endedHandler,
-            SharedCredentials & creds,
-            Args const &... args) {
-        auto credential = creds.lock().credentials.credential;
-        Json connectionInit{ { "type", "connection_init" },
-                             { "payload", Json::object({ { "X-Credential", std::move(credential) } }) } };
-        auto requestJson = OperationField::request(args...);
-        Json connectionParams = Json::object({ { "type", "start" }, { "payload", requestJson } });
+    template <typename OperationField>
+    class GraphqlSubscription : public std::enable_shared_from_this<GraphqlSubscription<OperationField>> {
+    public:
+        template <typename... Args>
+        GraphqlSubscription(
+                WebsocketClient & client,
+                SharedCredentials & creds,
+                std::string label,
+                std::function<void(caffql::GraphqlResponse<typename OperationField::ResponseData>)> messageHandler,
+                std::function<void(WebsocketClient::ConnectionEndType)> endedHandler,
+                Args const &... args)
+            : client(client)
+            , creds(creds)
+            , label(std::move(label))
+            , messageHandler(std::move(messageHandler))
+            , endedHandler(std::move(endedHandler)) {
+            auto requestJson = OperationField::request(args...);
+            connectionParams = Json::object({ { "type", "start" }, { "payload", requestJson } });
+        }
 
-        auto opened = [init = std::move(connectionInit), params = std::move(connectionParams), &client](
-                              WebsocketClient::Connection connection) {
-            client.sendMessage(connection, init.dump());
-            client.sendMessage(connection, params.dump());
-        };
+        ~GraphqlSubscription() { disconnect(); }
 
-        auto ended = [=](WebsocketClient::Connection connection, WebsocketClient::ConnectionEndType endType) {
-            if (endedHandler) {
-                endedHandler(endType);
+        void connect() {
+            disconnect();
+
+            auto credential = creds.lock().credentials.credential;
+            Json connectionInit{ { "type", "connection_init" },
+                                 { "payload", Json::object({ { "X-Credential", std::move(credential) } }) } };
+
+            std::weak_ptr<GraphqlSubscription> weakThis = this->shared_from_this();
+
+            auto & client = this->client;
+            auto opened = [init = std::move(connectionInit), weakThis](WebsocketClient::Connection connection) {
+                if (auto strongThis = weakThis.lock()) {
+                    strongThis->client.sendMessage(connection, init.dump());
+                    strongThis->client.sendMessage(connection, strongThis->connectionParams.dump());
+                }
+            };
+
+            auto ended = [weakThis](
+                                 WebsocketClient::Connection connection, WebsocketClient::ConnectionEndType endType) {
+                if (auto strongThis = weakThis.lock()) {
+                    if (strongThis->endedHandler) {
+                        strongThis->endedHandler(endType);
+                    }
+                }
+            };
+
+            auto messageReceived = [weakThis](WebsocketClient::Connection connection, std::string const & message) {
+                Json json;
+                try {
+                    json = Json::parse(message);
+                } catch (...) {
+                    LOG_ERROR("Failed to parse graphql subscription message");
+                    return;
+                }
+
+                auto typeIt = json.find("type");
+                if (typeIt == json.end() || *typeIt != "data") {
+                    return;
+                }
+
+                caffql::GraphqlResponse<typename OperationField::ResponseData> response;
+
+                try {
+                    response = OperationField::response(json.at("payload"));
+                } catch (std::exception const & error) {
+                    LOG_ERROR("Failed to unpack graphql subscription message: %s", error.what());
+                    return;
+                }
+
+                if (auto errors = get_if<std::vector<caffql::GraphqlError>>(&response)) {
+                    // Refresh credentials if needed
+                    bool shouldRefresh = false;
+                    for (auto const & error : *errors) {
+                        // TODO: Use a more robust way to tell that we need to refresh credentials
+                        if (error.message.find("credential") != std::string::npos) {
+                            shouldRefresh = true;
+                            break;
+                        }
+                    }
+
+
+                    if (shouldRefresh) {
+                        if (auto strongThis = weakThis.lock()) {
+                            if (refreshCredentials(strongThis->creds)) {
+                                strongThis->connect();
+                                return;
+                            }
+                        }
+                    }
+                }
+
+                if (auto strongThis = weakThis.lock()) {
+                    if (strongThis->messageHandler) {
+                        strongThis->messageHandler(response);
+                    }
+                }
+            };
+
+            connection = client.connect(REALTIME_GRAPHQL_URL, label, opened, ended, messageReceived);
+        }
+
+    private:
+        WebsocketClient & client;
+        SharedCredentials & creds;
+        std::string label;
+        Json connectionParams;
+        optional<WebsocketClient::Connection> connection;
+        std::function<void(caffql::GraphqlResponse<typename OperationField::ResponseData>)> messageHandler;
+        std::function<void(WebsocketClient::ConnectionEndType)> endedHandler;
+
+        void disconnect() {
+            if (connection) {
+                client.close(std::move(*connection));
+                connection.reset();
             }
-        };
-
-        auto messageReceived = [=](WebsocketClient::Connection connection, std::string const & message) {
-            Json json;
-            try {
-                json = Json::parse(message);
-            } catch (...) {
-                LOG_ERROR("Failed to parse graphql subscription message");
-                return;
-            }
-
-            auto typeIt = json.find("type");
-            if (typeIt == json.end() || *typeIt != "data") {
-                return;
-            }
-
-            caffql::GraphqlResponse<typename OperationField::ResponseData> response;
-
-            try {
-                response = OperationField::response(json.at("payload"));
-            } catch (std::exception const & error) {
-                LOG_ERROR("Failed to unpack graphql subscription message: %s", error.what());
-                return;
-            }
-
-            if (messageHandler) {
-                messageHandler(response);
-            }
-        };
-
-        return client.connect(REALTIME_GRAPHQL_URL, label, opened, ended, messageReceived);
-    }
+        }
+    };
 
 } // namespace caff
