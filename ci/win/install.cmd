9ce3c9eb04f59213471c9d094e72c20995c212b2
Matthew Orlando <cogwheel@caffeine.tv>
2019-08-12 17:07:18 -0700

    Attempt appveyor fix
    Manually install VS extension missing from the VS2019 image

--- ci/win/install.cmd
+++ ci/win/install.cmd
@@ -10,6 +10,16 @@ IF NOT EXIST "%ROOT%\.cache" (
     MKDIR "%ROOT%\.cache"
 )
 
+SET VSIXPATH=%ROOT%\.cache\llvm.vsix
+
+ECHO Installing LLVM toolchain
+IF EXIST %ROOT%\.cache\obs_deps.zip (
+    curl -kL "%LLVM_EXTENSION%" -f --retry 5 -o "%VSIXPATH%" -z "%VSIXPATH%"
+) ELSE (
+    curl -kL "%LLVM_EXTENSION%" -f --retry 5 -o "%VSIXPATH%"
+)
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\VSIXInstaller.exe" /q /a "%VSIXPATH%"
+
 ECHO Updating libcurl...
 IF EXIST %ROOT%\.cache\obs_deps.zip (
     curl -kL "%OBS_DEPENDENCIES_URL%" -f --retry 5 -o "%ROOT%\.cache\obs_deps.zip" -z "%ROOT%\.cache\obs_deps.zip"
